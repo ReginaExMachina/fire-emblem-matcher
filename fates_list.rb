93138760db949e3afca1b30a284008f4422348df
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-05 00:55:20 -0400

    Update fates_list.rb

--- fates_list.rb
+++ fates_list.rb
@@ -1,6 +1,6 @@
 # Public: Fire Emblem Fates characters and their preferences.
 # 
-# For fire-emblem-matcher, does not include none.
+# For fe_matcher, does not include none.
 
 prefs = {
   # GENTLEMEN
