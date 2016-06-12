30749c3a916d7e5f7c8ecbe23633845d128849af
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-12 00:07:51 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,6 +1,23 @@
+def branching()
+  #Test, need input/error handling
+	choice = raw_input('Which game?')
+	if choice == 'a':
+		from awakening import *
+	elif choice == 'b':
+		from birthright import *
+	elif choice == 'c':
+		from conquest import *
+	elif choice == 'r':
+		from relevations import *
+	else:
+		print('NA') 
+
 title = 'Fire Emblem Matchmaker\n\n'
 options = 'Fates Conquest [C] | Birthright [B] | Relevations [R] or Awakening [A]'
 
 menu_text = title + options
 
 print(menu_text.center(800))
+
+#branching()
+#printout_results()
