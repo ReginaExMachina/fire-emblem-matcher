2ebeb42c28a68c7113b44131b3865ab5abfe83cf
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-11 23:58:28 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,6 @@
+title = 'Fire Emblem Matchmaker\n\n'
+options = 'Fates Conquest [C] | Birthright [B] | Relevations [R] or Awakening [A]'
+
+menu_text = title + options
+
+print(menu_text.center(800))
