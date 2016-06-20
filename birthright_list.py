728e7f4586ee62a430516751af06e32fb404dff9
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-20 19:40:39 -0400

    Update birthright_list.py

--- birthright_list.py
+++ birthright_list.py
@@ -5,31 +5,35 @@
 
 # INITIALIZE THE CHARACTERS' DATA
 
+
+########################################################################
+
+# INITIALIZE THE CHARACTERS' DATA
+
 parent_prefs = {
-    'tsubaki': ['luna', 'hinoka', 'azura', 'felicia', 'hana', 'oboro'],
-    'nishiki': ['azura', 'sakura', 'hana', 'mozu'],
-    'jakob': ['azura', 'sakura', 'nyx'],
-    'silas': ['luna', 'effie', 'hinoka'],
-    'kaze': ['mozu', 'beruka', 'rinkah'],
-    'ryoma': ['kagero', 'orochi', 'camilla'],
-    'takumi': ['azura', 'felicia', 'elise'],
-    'saizo': ['orochi', 'beruka', 'mozu'],
-    'hinata': ['hana', 'hinoka', 'setsuna'],
-    'azama': ['effie', 'felicia', 'setsuna', 'beruka'],
-    'hayato': ['nyx', 'sakura', 'oboro']
+    'Tsubaki': ['Hinoka', 'Azura', 'Felicia', 'Hana', 'Oboro'],
+    'Nishiki': ['Azura', 'Sakura', 'Hinoka', 'Hana', 'Mozu', 'Setsuna'],
+    'Jakob': ['Azura', 'Sakura', 'Oboro'],
+    'Silas': ['Oboro', 'Hinoka'],
+    'Kaze': ['Mozu', 'Azura', 'Rinkah'],
+    'Ryoma': ['Kagero', 'Rinkah', 'Orochi'],
+    'Takumi': ['Azura', 'Felicia'],
+    'Saizo': ['Orochi', 'Beruka', 'Mozu'],
+    'Hinata': ['Hana', 'Hinoka', 'Setsuna'],
+    'Azama': ['Felicia', 'Setsuna'],
+    'Hayato': ['Sakura', 'Oboro']
 }
 
 partner_prefs = {
-    'azura': ['laslow', 'jakob', 'nishiki', 'takumi'],
-    'felicia': ['leo', 'takumi', 'jakob', 'azama'],
-    'hinoka': ['leo', 'xander', 'hinata', 'silas'],
-    'sakura': ['xander', 'jakob', 'leo', 'hayato', 'nishiki'],
-    'kagero': ['ryoma', 'saizo', 'jakob'],
-    'peri': ['laslow', 'benny', 'xander'],
-    'hana': ['ryoma', 'keaton', 'hinata', 'tsubaki'],
-    'oboro': ['takumi', 'tsubaki', 'hayato'],
-    'setsuna': ['hinata', 'jakob', 'azama'],
-    'rinkah': ['silas', 'kaze', 'nishiki', 'ryoma'],
-    'mozu': ['nishiki', 'niles', 'kaze'],
-    'orochi': ['saizo', 'ryoma', 'silas', 'jakob'],
+    'Azura': ['Jakob', 'Nishiki', 'Takumi', 'Kaze'],
+    'Felicia': ['Takumi', 'Jakob', 'Azama'],
+    'Hinoka': ['Hinata', 'Silas', 'Nishiki'],
+    'Sakura': ['Jakob', 'Hayato', 'Nishiki'],
+    'Kagero': ['Ryoma', 'Saizo', 'Jakob'],
+    'Hana': ['Ryoma', 'Hinata', 'Tsubaki'],
+    'Oboro': ['Takumi', 'Silas', 'Hinata', 'Tsubaki', 'Hayato'],
+    'Setsuna': ['Hinata', 'Jakob', 'Azama', 'Nishiki'],
+    'Rinkah': ['Silas', 'Kaze', 'Nishiki', 'Ryoma'],
+    'Mozu': ['Nishiki', 'Kaze'],
+    'Orochi': ['Saizo', 'Ryoma', 'Silas', 'Jakob'],
     }
