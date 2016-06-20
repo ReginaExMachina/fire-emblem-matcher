6de4b60c2a25dd0b3e5bbf23663791955472da23
Rachel <we.love.rachel.day.lots@gmail.com>
2016-06-20 19:33:18 -0400

    Update branching.py

--- branching.py
+++ branching.py
@@ -7,3 +7,15 @@
 # 
 
 ########################################################################
+
+choice = raw_input('Which game?')
+if choice == 'a':
+    from awakening_list import *
+elif choice == 'b':
+    from birthright_list import *
+elif choice == 'c':
+    from conquest_list import *
+elif choice == 'r':
+    from relevations_list import *
+else:        
+    print('NA') 
